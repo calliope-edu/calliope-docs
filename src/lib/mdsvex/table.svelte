464a3ade574f31d8ad1ec8a6108e50bad0034991
JW <Amerlander@users.noreply.github.com>
2022-08-26 18:46:51 +0200

    update tables

--- src/lib/mdsvex/table.svelte
+++ src/lib/mdsvex/table.svelte
@@ -3,7 +3,7 @@
         margin: 0;
     }
     div :global(table) {
-        @apply w-full text-xs text-left text-gray-500 dark:text-gray-400;
+        @apply w-full relative text-sm text-left text-gray-500 dark:text-gray-400;
     }
 
     div :global(thead) {
@@ -15,7 +15,7 @@
     }
 
     div :global(tbody tr) {
-        @apply text-xs bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600;
+        @apply bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600;
     }
 
     div :global(tbody th) {
@@ -23,12 +23,14 @@
     }
 
     div :global(tbody td) {
-        @apply py-4 px-6;
+        @apply py-4 px-6 align-middle;
     }
 </style>
 
 
 
-<div class="overflow-x-auto shadow-md sm:rounded-lg">
-<slot />
+<div class="overflow-auto shadow-md sm:rounded-lg">
+    <table>
+        <slot />
+    </table>
 </div>
