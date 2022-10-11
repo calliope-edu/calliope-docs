c8472a5bd9b6c12e5dbf1053113e02e9fc47929b
Jasper <jasper.precht@web.de>
2022-10-11 15:19:13 +0200

    Icon Komponente

---
+++ src/lib/components/elements/Icon.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Icon from '~icons/ri/*';
+
+	export let iconContent = '';
+	export let inverse = false;
+	export let color = '';
+	export let size = 'sm';
+
+	$: isNumber = /^\d+$/.test(iconContent);
+</script>
+
+<div class="p-3 icon {inverse ? `text-white bg-${color} rounded-${size}` : `text-${color}`}">
+	{#if isNumber}
+		{iconContent}
+	{:else}
+		<Icon /> <!-- get Icon URL -->
+	{/if}
+</div>
+
+<style>
+</style>
