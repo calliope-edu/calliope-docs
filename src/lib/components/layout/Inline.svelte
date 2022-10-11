4d3d1a6e052c36833d213eb6e4e865128c49e1fc
Jasper <jasper.precht@web.de>
2022-10-11 14:47:21 +0200

    Inline/Flex-Item

---
+++ src/lib/components/layout/Inline.svelte
@@ -0,0 +1,10 @@
+<script>
+    export let align = 'left';
+</script>
+
+<div class="container flex justify-{align} items-center">
+<slot></slot></div>
+
+<style>
+
+</style>
---
+++ src/stories/Layout/Inline.stories.svelte
@@ -0,0 +1,41 @@
+<script>
+    import ProseDecorator from './../Decorators/ProseDecorator.svelte';
+      import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
+  
+      import Inline from '../../lib/components/layout/Inline.svelte';
+      import Button from '../../lib/components/elements/Button.svelte';
+  
+  </script>
+  
+  <Meta 
+      title="Layout/Inline" 
+      component={Inline}
+      argTypes={{
+          align: { control: 'text', description: 'left, right, end' },
+      }}
+  />
+  
+  <Template let:args>
+      <Inline {...args}>
+      <div></div>
+      <div></div>
+      <div></div>
+      </Inline>
+  </Template>
+
+  <Story name="Elemente Anordnen">
+    <Inline align='left'>
+        <Button color='rosa'>Rosa</Button>
+        <h1>Headline H1</h1>
+        </Inline>
+
+        <Inline align='center'>
+            <Button color='rosa'>Rosa</Button>
+            <h1>Headline H2</h1>
+            </Inline>
+
+            <Inline align='end'>
+                <Button color='rosa'>Rosa</Button>
+                <h1>Headline H1</h1>
+                </Inline>
+</Story>
