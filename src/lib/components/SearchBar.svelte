39b0b748fd2e2815d96a69c21c92ccdc4995a341
Juri Wolf <Amerlander@users.noreply.github.com>
2024-01-14 02:16:52 +0100

    search css

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -17,7 +17,7 @@
           // ... 
           query_by:
             'hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,hierarchy.lvl6,content,embedding',
-          vector_query: 'embedding:([], k: 5, distance_threshold: 1.0, alpha: 0.2)' // Optional vector search fine-tuning
+             vector_query: 'embedding:([], k: 5, distance_threshold: 1.0, alpha: 0.2)' // Optional vector search fine-tuning
         },
       });
     });
@@ -30,7 +30,7 @@
 </div>
 
 
-<style>
+<style lang="scss">
     input {
       width: 300px;
       padding: 10px;
@@ -38,6 +38,31 @@
       border-radius: 5px;
       outline: none;
     }
+
+
+:global(.algolia-autocomplete .ds-dropdown-menu) {
+  // width: 500px;
+}
+
+:global(.algolia-autocomplete .typesense-docsearch-suggestion--category-header) {
+  // color: darkgray;
+}
+
+:global(.algolia-autocomplete .typesense-docsearch-suggestion--subcategory-column) {
+  // color: gray;
+}
+
+:global(.algolia-autocomplete .typesense-docsearch-suggestion--title) {
+  font-weight: bold;
+}
+
+:global(.algolia-autocomplete .typesense-docsearch-suggestion--text) {
+  color: gray;
+}
+
+:global(.algolia-autocomplete .typesense-docsearch-suggestion--highlight) {
+  color: var(--color-calliope);
+}
   
   </style>
   
