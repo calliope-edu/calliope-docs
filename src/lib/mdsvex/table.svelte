74217260e84840dbe561668d7bfdf99c15d5798c
Juri Wolf <Amerlander@users.noreply.github.com>
2024-01-20 10:18:11 +0100

    add compact style to tables

--- src/lib/mdsvex/table.svelte
+++ src/lib/mdsvex/table.svelte
@@ -1,32 +1,14 @@
+<div class="overflow-auto">
+    <table class="ui celled compact striped table">
+        <slot />
+    </table>
+</div>
+
 <style lang="scss">
-    // div :global(*) {
-    //     margin: 0;
-    // }
-    // div :global(table) {
-    // }
 
     div :global(thead) {
         position: sticky; 
         top: 75px; 
     }
 
-    // div :global(thead th) {
-    // }
-
-    // div :global(tbody tr) {
-    // }
-
-    // div :global(tbody th) {
-    // }
-
-    // div :global(tbody td) {
-    // }
 </style>
-
-
-
-<div class="overflow-auto">
-    <table class="ui celled striped table">
-        <slot />
-    </table>
-</div>
