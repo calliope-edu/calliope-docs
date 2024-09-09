570d1276cb87561bc45489de565c6e2ae0e96fe8
Amerlander <Amerlander@users.noreply.github.com>
2024-09-09 19:49:47 +0200

    add uf2 to vite config

--- vite.config.js
+++ vite.config.js
@@ -24,5 +24,5 @@ export default defineConfig({
 			compiler: 'svelte',
 		  }),
 		],
-		assetsInclude: ['**/*.hex', '**/*.xml', '**/*.pdf'],
+		assetsInclude: ['**/*.hex', '**/*.xml', '**/*.pdf', '**/*.uf2'],
 });
